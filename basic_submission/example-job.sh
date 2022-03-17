efc1249d7271a92151a80cac3f6aa44eb415e0d2
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2022-03-17 21:00:25 +0000

    Update example job

--- basic_submission/example-job.sh
+++ basic_submission/example-job.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -l
 
+# Request ten minutes of wallclock time (format hours:minutes:seconds).
+#$ -l h_rt=0:10:0
+
+# Request 1 gigabyte of RAM (must be an integer followed by M, G, or T)
+#$ -l mem=10G
+
+# Set the name of the job.
+#$ -N PHAS0100
+
+# Set the working directory to the submission directory.
 #$ -cwd
 
 echo -n "This script is running on "
