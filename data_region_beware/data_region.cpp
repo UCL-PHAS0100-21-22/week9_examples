4ab583768e9510671f99581cb302b627a85f412d
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2022-03-17 22:09:15 +0000

    Change kernels to parallel loop

--- data_region_beware/data_region.cpp
+++ data_region_beware/data_region.cpp
@@ -5,7 +5,7 @@ int main() {
 
   #pragma acc data copy(A)
   {
-    #pragma acc kernels
+    #pragma acc parallel loop
     for( int iter = 1; iter < 1000 ; iter++){
       A[iter] = 1.0;
     }
--- data_region_beware/no_data_region.cpp
+++ data_region_beware/no_data_region.cpp
@@ -3,7 +3,7 @@
 int main() {
   float A[1000];
 
-  #pragma acc kernels
+  #pragma acc parallel loop
   for( int iter = 1; iter < 1000 ; iter++){
     A[iter] = 1.0;
   }
